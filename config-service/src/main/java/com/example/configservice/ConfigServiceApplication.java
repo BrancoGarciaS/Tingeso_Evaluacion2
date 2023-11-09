43144913f9be886b55a286327654cdf38a7b5683
BrancoGarciaS <branco.garcia@usach.cl>
2023-11-09 00:28:40 -0300

    Corrección config-service

--- config-service/src/main/java/com/example/configservice/ConfigServiceApplication.java
+++ config-service/src/main/java/com/example/configservice/ConfigServiceApplication.java
@@ -2,10 +2,7 @@ package com.example.configservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.config.client.ConfigServerConfigDataLocationResolver;
 import org.springframework.cloud.config.server.EnableConfigServer;
-import org.springframework.context.annotation.Bean;
-import org.springframework.core.log.LogAccessor;
 
 @SpringBootApplication
 @EnableConfigServer
